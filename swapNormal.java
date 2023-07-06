1cc796b156a01eb6d265cf46a43b7b93c296f4e2
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-06 14:56:21 +0530

    normal swaping without passing numbers as arguments

---
+++ swapNormal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class swapNormal {
+    public static void main(String[] args) {
+        System.out.println("Enter num1: ");
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter num2: ");
+        int n2 = sc.nextInt();
+
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+
+        System.out.println("Numbers swaped\n"+"num1: "+n1+"\n"+"num2: "+n2);
+    }
+}
