3734afa09d89c4fd07b88a023805aa117a631558
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-06 14:43:08 +0530

    passing arguments

--- returnString.java
+++ returnString.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class returnString{
     public static void main(String[] args) {
-        String abc = greet();
+        System.out.println("Enter your name ");
+        Scanner inp = new Scanner(System.in);
+        String nme = inp.nextLine();
+
+        String abc = greet(nme);
         System.out.println(abc);
     }
-    static String greet(){
+    static String greet(String name){
 
-        String greeting = "How are YOU";
+        String greeting = "How are u "+name+ "?";
         return greeting;
     }
 }
