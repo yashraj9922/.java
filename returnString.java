cb64a15a850043c31785eecc0abc33b7478e3f2b
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-06 14:38:08 +0530

    returnString in java using method/function

---
+++ returnString.java
@@ -0,0 +1,11 @@
+public class returnString{
+    public static void main(String[] args) {
+        String abc = greet();
+        System.out.println(abc);
+    }
+    static String greet(){
+
+        String greeting = "How are YOU";
+        return greeting;
+    }
+}
