00e43a96ea9220a0ed56b23ad902cfda01a8f5a8
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-06 09:57:47 +0530

    first commit

---
+++ sumMethod.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sumMethod {
+    void sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a+b;
+
+        System.out.println("Sum of two numbers is "+sum);
+    }
+
+    public static void main(String[] args) {
+        sumMethod obj = new sumMethod();
+        obj.sum();
+    }
+}
