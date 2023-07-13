9829d2f3a0a2dc9a82839bece95207ac4c70c750
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-13 16:03:22 +0530

    2d array input

---
+++ array2dInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class array2dInput {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+
+        System.out.println("Enter the number of columns: ");
+        int columns = sc.nextInt();
+
+        int [][]arr = new int[rows][columns];
+
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<columns; j++){
+                System.out.println("Element with index ["+i+","+j+"] is: ");
+                arr[i][j] = sc.nextInt();
+
+            }
+        }
+        printArray(rows, columns, arr);
+    }
+    static void printArray(int row, int column, int [][]arr){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
