90b91a5dbd1bbdc284f4faed4e50955fbad00b14
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-03 12:45:32 +0530

    insertion sort

--- dsa_revise/insertionSort.java
+++ dsa_revise/insertionSort.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+// apni kaksha
 public class insertionSort {
-    
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 2, 1, 0 };
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j > -1 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
