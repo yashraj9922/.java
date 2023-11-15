ba194e598d74e660f8b4866dd72694b1d990a9cc
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-11-15 22:42:27 +0530

    packages

--- oops/src/intro/Student.java
+++ oops/src/intro/Student.java
@@ -1,3 +1,4 @@
+package intro;
 class Student {
     String name;
     int rollNo;
--- oops/src/intro/WrapperExample.java
+++ oops/src/intro/WrapperExample.java
@@ -1,3 +1,4 @@
+package intro;
 public class WrapperExample {
     public static void main(String[] args) {
         int a = 10;
