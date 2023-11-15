074cfa09a93ffc3f6d59a962842b78eda892b8f8
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-11-15 22:17:57 +0530

    final keyword

--- oops/src/WrapperExample.java
+++ oops/src/WrapperExample.java
@@ -3,7 +3,7 @@ public class WrapperExample {
         int a = 10;
         int b = 20;
 
-        Integer num = 45;
+        // Integer num = 45;
 
         swap1(a, b);
         // we know that this will not swap the numbers because in java
@@ -12,9 +12,12 @@ public class WrapperExample {
 
         Integer x = 10;
         Integer y = 20;
-        swap1(x, y);
+        swap2(x, y);
         System.out.println(a + " " + b);
         // still values are not swap...
+        // public final class Integer extends Number...
+        // Interger is a final class....u can not modify value when varibale is declared
+        // with final keyword
 
     }
 
@@ -30,5 +33,31 @@ public class WrapperExample {
         Integer temp = a;
         a = b;
         b = temp;
+
+        final A yash = new A("yashraj");
+        yash.name = "new name"; // this can be done
+        
+        // when a non-primitive is final u cannot reassign it...
+        // yash = new A("new object");-->cannot be done
+    }
+}
+
+// final keyword
+// always initialised while declaring
+// final gaurentee immutability when only when instance variable are primitive data types
+// in case of non primitive data types value can be changed but u cannot ressigned it
+/*
+ * final Student yash = new Student("yashraj");
+ * yash.name = "new name";
+ * 
+ * yash = new object....this is not allowed
+ */
+
+class A {
+    final int num = 10;
+    String name;
+
+    public A(String name) {
+        this.name = name;
     }
 }
