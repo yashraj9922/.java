bdaf3a087bf163aeb1b078edee151dda96918c20
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-11-16 15:21:02 +0530

    static n non static

--- oops/src/staticExample/main.java
+++ oops/src/staticExample/main.java
@@ -11,4 +11,30 @@ public class main {
         System.out.println(yash.population);// 2 humans
 
     }
+
+    // this is independent of an object
+    // this does not belong to any instance
+    static void fun() {
+        // greeting();--> this is not accesible...
+        // a static method can only access a static data...it cannot access non-static
+        // data
+        // hence without specifing a instance we cannot use it
+    }
+
+    // non static method
+    // as we know that static data is independent of any object....and something
+    // that is not static , belongs to an object
+    // this belongs to an instance
+    void greeting() {
+        System.out.println("Hello World");
+    }
+
+    static void greetMe() {
+        main obj = new main();
+        obj.greeting();
+        // now as greeting() has a specified instance now we can use it
+
+        // you cannot access non static stuff without referencing their instances in a
+        // static context
+    }
 }
